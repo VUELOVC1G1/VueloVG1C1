9856971def953199f6623bf9b09699e723f01f0a
JoseCedeño <81100807+Jose-22-ced@users.noreply.github.com>
2022-06-02 17:51:54 -0500

    Initial commit

---
+++ src/main/java/com/complexivo3/vuelovg1c1/VueloVg1C1Application.java
@@ -0,0 +1,13 @@
+package com.complexivo3.vuelovg1c1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueloVg1C1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueloVg1C1Application.class, args);
+    }
+
+}
